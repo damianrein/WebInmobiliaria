3b75c9cfde84937e3a5d28e13a6ce1c02db96b3e
damianrein <damianrein464@gmail.com>
2024-10-15 21:58:38 -0300

    1.0.0

--- src/app/components/loginpage/loginpage.component.ts
+++ src/app/components/loginpage/loginpage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-loginpage',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginpageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(form: NgForm){
+    const username = form.value.username;
+    const password = form.value.password;
+    return this.authService.login(username,password).subscribe({
+      next : (response) => {
+        const token = response.token;
+
+        localStorage.setItem("jwtToken", token);
+
+        this.router.navigate(['/home']);
+      },
+      error: (error) =>{
+        console.error('Error en el login', error);
+        let errorMessage = 'Error en el inicio de sesión';
+        
+        if (error.status === 401) {
+          errorMessage = 'Credenciales incorrectas. Por favor, inténtalo de nuevo.';
+        } else if (error.status === 0) {
+          errorMessage = 'No se puede conectar al servidor. Intenta más tarde.';
+        }
+
+        alert(errorMessage);
+      }
+  
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  apiUrl = "localhost:8080/auth";
+
+  logged = false;
+
+  constructor(private http: HttpClient) { }
+
+  login(username: String, password: String):Observable<any>{
+    this.logged = true;
+    return this.http.post<any>('${this.apiUrl}/login',{username, password});
+  }
+
+  isLogged(){
+    return this.logged;
+  }
+
+  logout(){
+    this.logged = false;
+  }
 }
